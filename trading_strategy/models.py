cc9a4ca1094214961a12da3e2d8e3425319cd51d
darkstorn <inikishin@gmail.com>
2021-12-12 11:19:30 +0300

    add Trading Strategy models

--- trading_strategy/models.py
+++ trading_strategy/models.py
@@ -1,21 +1,69 @@
+"""
+Trading Strategy django app
+"""
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 from quote.models import Ticker, Timeframe
 
+
 class TradingStrategy(models.Model):
-    code = models.CharField(max_length=100, unique=True)
+    """
+    Trading strategy model
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     fullname = models.CharField(max_length=1000)
     description = models.CharField(max_length=1000, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    timeframe = models.ForeignKey(Timeframe, on_delete=models.CASCADE, blank=True, null=True)
+    ticker = models.ForeignKey(Ticker, on_delete=models.CASCADE, blank=True, null=True)
+    filters = models.TextField()
+    entrySignals = models.TextField()
+    exitSignals = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __repr__(self):
-        return f'{self.code} (id: {self.pk})'
+        return f'{self.fullname} (id: {self.id})'
 
     def __str__(self):
-        return f'{self.code}'
+        return f'{self.fullname}'
+
+
+class TradingStrategyTestResult(models.Model):
+    """
+    Trading strategy test result model
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    trading_strategy = models.ForeignKey(TradingStrategy, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    timeframe = models.ForeignKey(Timeframe, on_delete=models.CASCADE, blank=True, null=True)
+    ticker = models.ForeignKey(Ticker, on_delete=models.CASCADE, blank=True, null=True)
+    filters = models.TextField()
+    entrySignals = models.TextField()
+    exitSignals = models.TextField()
+    test_result = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __repr__(self):
+        return f'{self.trading_strategy} (created_at: {self.created_at})'
+
+    def __str__(self):
+        return f'{self.trading_strategy} (created_at: {self.created_at})'
 
 
 class Signal(models.Model):
-    ticker = models.ForeignKey(Ticker, on_delete=models.CASCADE)
-    timeframe = models.ForeignKey(Timeframe, on_delete=models.CASCADE)
-    datetime = models.DateTimeField()
+    """
+    Signal model
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     trading_strategy = models.ForeignKey(TradingStrategy, on_delete=models.CASCADE)
-    value = models.FloatField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    datetime = models.DateTimeField()
+    feature_values = models.TextField() # Текущее значение всех фичей для проверки
+    signal = models.FloatField()
+
+    def __repr__(self):
+        return f'{self.trading_strategy} (signal: {self.signal})'
+
+    def __str__(self):
+        return f'{self.trading_strategy} (signal: {self.signal})'
